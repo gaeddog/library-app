8ac8264360da6b7951bf185280053e8ede858272
gaeddog <gaeddog@gmail.com>
2024-06-22 16:58:38 +0900

    2

--- src/main/java/com/group/libraryapp/UserConfiguration.java
+++ src/main/java/com/group/libraryapp/UserConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 public class UserConfiguration {
 
 //    @Bean  /test01
+    // test02
 //    public UserRepository userRepository(JdbcTemplate jdbcTemplate) {
 //        return new UserRepository(jdbcTemplate);
 //    }
